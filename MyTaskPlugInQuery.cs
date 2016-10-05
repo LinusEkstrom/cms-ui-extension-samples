2102aa78a9f8889dda62a5e9c866a99d181804dc
LinusEkstrom <linus.ekstrom@niteco.com>
2016-10-05 09:46:03 +0700

    Added a plug-in for tasks gadget
    Added an example on how to plug into the tasks gadget.

---
+++ MyTaskPlugInQuery.cs
@@ -0,0 +1,83 @@
+﻿using EPiServer.Core;
+using EPiServer.Framework.Localization;
+using EPiServer.Globalization;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell.ContentQuery;
+using EPiServer.UI.Report.Reports;
+using System;
+using System.Collections.Generic;
+
+namespace EPiServer.Cms.Shell.UI.Rest.ContentQuery
+{
+    [ServiceConfiguration(typeof(IContentQuery))]
+    public class MyTaskPlugIn : ContentQueryBase
+    {
+        private readonly LocalizationService _localizationService;
+        private readonly IContentRepository _contentRepository;
+        private readonly ExpiredPagesData _expiredPagesData;
+
+        public MyTaskPlugIn(LocalizationService localizationService,
+            IContentQueryHelper queryHelper,
+            IContentRepository contentRepository,
+            ExpiredPagesData expiredPagesData) : base(contentRepository, queryHelper)
+        {
+            _localizationService = localizationService;
+            _contentRepository = contentRepository;
+            _expiredPagesData = expiredPagesData;
+        }
+
+        public override string Name
+        {
+            get { return "expiredpages"; }
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return "Expired pages";
+                //Should be localized in a production scenario.
+                //return _localizationService.GetString("/add/your/path/to/translation");
+            }
+        }
+
+        public override IEnumerable<string> PlugInAreas
+        {
+            //There is currently no static string for this plug-in area.
+            get { return new string[] { "editortasks" }; }
+        }
+
+        public override int SortOrder
+        {
+            get { return 105; }
+        }
+
+        public override bool VersionSpecific
+        {
+            //You can configure if the UI should load a specific version or just use normal logic to find the best/latest version.
+            get { return false; }
+        }
+
+        protected override IEnumerable<IContent> GetContent(ContentQueryParameters parameters)
+        {
+            int rowCount;
+
+            var expiredPages = _expiredPagesData.GetPages(
+                ContentReference.StartPage,
+               DateTime.MinValue,
+               DateTime.Now,
+               ContentLanguage.PreferredCulture.Name,
+               false,
+               String.Empty,
+               50,
+               0,
+               out rowCount
+                );
+
+            var list = new List<IContent>();
+            list.AddRange(expiredPages);
+
+            return list;
+        }
+    }
+}
